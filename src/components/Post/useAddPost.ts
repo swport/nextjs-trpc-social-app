d1a5d20640bbe0cedfda7a953774e5a34cc73eaf
Sumit Wadhwa <wadhwa.sumit007@yahoo.com>
2024-03-29 00:53:40 +0530

    feature: updates post cache when post is added

--- src/components/Post/useAddPost.ts
+++ src/components/Post/useAddPost.ts
@@ -1,12 +1,18 @@
 import React from "react";
 import { trpc } from "@/utils/trpc";
+import { getQueryKey } from "@trpc/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 
 // for temporary purpose - just to generate an image
 import { faker } from "@faker-js/faker";
 
 const useAddPost = () => {
 	const addPostMutation = trpc.post.add.useMutation();
-	const utils = trpc.useContext();
+	// const utils = trpc.useContext();
+
+	// trpc.useContext utils doesn't work as expected, gives undefined for prevData
+	// have to rely on native implementation
+	const queryClient = useQueryClient();
 
 	const [error, setError] = React.useState<string>();
 	const [isLoading, setLoading] = React.useState(false);
@@ -24,7 +30,30 @@ const useAddPost = () => {
 		addPostMutation.mutate(
 			{ title, image },
 			{
-				onSuccess: ({ results: post }) => {
+				onSuccess: (post) => {
+					// update cache when comment is added
+					const postListKey = getQueryKey(
+						trpc.post.fetchAll,
+						{ limit: 15 },
+						"infinite"
+					);
+
+					// TODO: update types
+					const queryData =
+						queryClient.getQueryData<Record<string, any>>(postListKey);
+
+					if (queryData && typeof queryData["pages"] !== "undefined") {
+						// prepend comment to the first page
+						const firstPage = queryData["pages"][0] as { data: any[] };
+
+						if (firstPage) {
+							queryData["pages"][0]["data"] = [post, ...firstPage.data];
+
+							queryClient.setQueryData(postListKey, {
+								...queryData,
+							});
+						}
+					}
 					setPostAdded(true);
 				},
 				onError: () => {
